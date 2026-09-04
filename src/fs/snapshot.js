c3a53b56ee992b48147a073147805f12bcecd582
Тимофей Поповский <penlist127@gmail.com>
2026-09-04 16:24:23 +0300

    snapshot v2

--- src/fs/snapshot.js
+++ src/fs/snapshot.js
@@ -6,79 +6,29 @@ import { fileURLToPath } from "node:url";
 const snapshot = async () => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
+
   const rootPath = path.resolve(__dirname, "../../workspace");
+  const files = await fs.readdir(rootPath, { recursive: true });
 
-  // вариант с промисами
-  const jsonPromise = { rootPath, entries: [] };
-  const scanPromise = async (_path) => {
-    let files;
-    try {
-      files = await fs.readdir(_path);
-    } catch (_) {
-      throw new Error("FS operation failed");
+  const entries = [];
+  for (const file of files) {
+    const filePath = path.resolve(rootPath, file);
+    const stat = await fs.stat(filePath);
+
+    if (stat.isFile()) {
+      const content = await fs.readFile(filePath, "utf-8");
+      entries.push({ path: file, type: "file", size: stat.size, content });
+    } else {
+      entries.push({ path: file, type: "directory" });
     }
-    for (const file of files) {
-      // path.join, чтоб работало и в юникс и в винде. \ /.
-      const filePath = path.join(_path, file);
-      const relativePath = path.relative(rootPath, filePath);
-      const stat = await fs.stat(filePath);
-      if (stat.isFile()) {
-        const content = await fs.readFile(filePath, { encoding: "base64" });
-        jsonPromise.entries.push({
-          path: relativePath,
-          content,
-          type: "file",
-          size: stat.size,
-        });
-      } else {
-        jsonPromise.entries.push({ path: relativePath, type: "directory" });
-        await scanPromise(filePath);
-      }
-    }
+  }
+
+  const result = {
+    rootPath,
+    entries,
   };
-  await scanPromise(rootPath);
 
-  // это если я правильно понял, что надо класть snapshot.json рядом с папкой workspace, а не внутрь нее класть
-  const snapshotPath = path.join(path.dirname(rootPath), "snapshot.json");
-  await fs.writeFile(snapshotPath, JSON.stringify(jsonPromise));
-
-  // вариант с колбэками
-  // const jsonCB = { rootPath, entries: [] };
-  // const scanCB = (_path) => {
-  //   fsCB.readdir(_path, {}, (err, files) => {
-  //     if (err) throw new Error("FS operation failed");
-
-  //     for (const file of files) {
-  //       const filePath = path.join(_path, file);
-  //       const relativePath = path.relative(rootPath, filePath);
-  //       fsCB.stat(filePath, (_err, stats) => {
-  //         if (stats.isFile()) {
-  //           fsCB.readFile(filePath, { encoding: "base64" }, (_err, data) => {
-  //             jsonCB.entries.push({
-  //               path: relativePath,
-  //               content: data,
-  //               type: "file",
-  //               size: stats.size,
-  //             });
-  //             const snapshotPath = path.join(
-  //               path.dirname(rootPath),
-  //               "snapshot.json",
-  //             );
-  //             fsCB.writeFile(
-  //               snapshotPath,
-  //               JSON.stringify(jsonCB),
-  //               (_err) => {},
-  //             );
-  //           });
-  //         } else {
-  //           jsonCB.entries.push({ path: relativePath, type: "directory" });
-  //           scanCB(filePath);
-  //         }
-  //       });
-  //     }
-  //   });
-  // };
-  // scanCB(rootPath);
+  await fs.writeFile("snapshot.json", JSON.stringify(result));
 };
 
 await snapshot();
