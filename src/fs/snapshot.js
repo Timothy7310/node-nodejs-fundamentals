7be5f0380d0bcfea6e83e44959fd37e305a6f731
Тимофей Поповский <penlist127@gmail.com>
2026-09-04 16:31:01 +0300

    refactor

--- src/fs/snapshot.js
+++ src/fs/snapshot.js
@@ -1,11 +1,9 @@
 import fs from "node:fs/promises";
 import fsCB from "node:fs";
 import path from "node:path";
-import { fileURLToPath } from "node:url";
 
 const snapshot = async () => {
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = path.dirname(__filename);
+  const __dirname = import.meta.dirname;
 
   const rootPath = path.resolve(__dirname, "../../workspace");
   const files = await fs.readdir(rootPath, { recursive: true });
