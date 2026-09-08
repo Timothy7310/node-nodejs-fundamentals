0b1dc2f26f8062e3533a884dfd87b3baf7539046
Тимофей Поповский <penlist127@gmail.com>
2026-09-08 19:40:50 +0300

    add restore.js

--- src/fs/restore.js
+++ src/fs/restore.js
@@ -1,8 +1,40 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import fs from "node:fs/promises";
+
+// todo:  rootPath from snapshot should be treated as metadata and must not affect restore destination. так и не понял че это значит
 const restore = async () => {
-  // Write your code here
-  // Read snapshot.json
-  // Treat snapshot.rootPath as metadata only
-  // Recreate directory/file structure in workspace_restored
+  const __dirname = import.meta.dirname;
+  const rootPath = path.resolve(__dirname, "../../");
+  const workspaceRestoredDir = path.resolve(rootPath, "workspace_restored");
+
+  try {
+    await fs.mkdir(workspaceRestoredDir);
+  } catch (err) {
+    if (err.code === "EEXIST") {
+      throw new Error("FS operation failed");
+    }
+  }
+
+  const snapshotPath = path.resolve(rootPath, "snapshot.json");
+  let snapshot;
+  try {
+    snapshot = JSON.parse(await fs.readFile(snapshotPath, "utf-8"));
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      throw new Error("FS operation failed");
+    }
+  }
+
+  for (const file of snapshot.entries) {
+    if (file.type === "file") {
+      const filePath = path.resolve(workspaceRestoredDir, file.path);
+      await fs.writeFile(filePath, atob(file.content));
+    } else if (file.type === "directory") {
+      const dirPath = path.resolve(workspaceRestoredDir, file.path);
+      await fs.mkdir(dirPath);
+    }
+  }
 };
 
 await restore();
